d1a51afaa98eee126c0c1b4d90b84199169ee12e
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-06-25 00:34:37 +0200

    Moved form generator function to class

--- src/selenium_injector/webdriver.py
+++ src/selenium_injector/webdriver.py
@@ -1,40 +1,42 @@
-import selenium.webdriver.chromium.webdriver
 from selenium.webdriver import ChromeOptions
 from selenium_injector.scripts.driverless import Driverless
 
 
-# noinspection PyDefaultArgument
-def Chrome(driverless_options: dict = {"port": None, "host": None}, base_driver=None, *args, **kwargs):
-    if not base_driver:
-        from selenium.webdriver import Chrome as base_driver
+class Base:
+    pass
 
-    class _Chrome(base_driver):
-        # noinspection PyDefaultArgument
-        def __init__(self, _driverless_options={"port": None, "host": None}, *_args, **_kwargs):
-            port = _driverless_options["port"]
-            host = _driverless_options["host"]
-            self.driverless = Driverless(port=port, host=host)
 
-            from selenium_injector.utils.utils import read
-            utils_js = read("files/js/utils.js")
+class Chrome(Base):
+    # noinspection PyDefaultArgument
+    def __init__(self, driverless_options={"port": None, "host": None}, base_driver=None, **kwargs):
 
-            if "options" not in _kwargs.keys():
-                _kwargs["options"] = ChromeOptions()
-            _kwargs["options"].add_argument(f'--load-extension={self.driverless.path}')
+        if not base_driver:
+            from selenium.webdriver import Chrome as base_driver
 
-            super().__init__(*_args, **_kwargs)
+        Chrome.__bases__ = (Chrome.__base__, base_driver,)
 
-            tab_index = self.window_handles.index(self.current_window_handle).__str__()
-            self.driverless.tab_user = "tab-" + tab_index
-            config = f"""
-                    var connection = new connector("{self.driverless.socket.host}", {self.driverless.socket.port}, "{self.driverless.tab_user}")
-                    connection.connect();
-                    """
-            self.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument",
-                                 {"source": "(function(){%s})()" % (utils_js + self.driverless.connection_js + config)})
+        port = driverless_options["port"]
+        host = driverless_options["host"]
+        self.driverless = Driverless(port=port, host=host)
 
-        def quit(self) -> None:
-            self.driverless.stop()
-            super().quit()
+        from selenium_injector.utils.utils import read
+        utils_js = read("files/js/utils.js")
 
-    return _Chrome(_driverless_options=driverless_options, *args, **kwargs)
+        if "options" not in kwargs.keys():
+            kwargs["options"] = ChromeOptions()
+        kwargs["options"].add_argument(f'--load-extension={self.driverless.path}')
+
+        super().__init__(**kwargs)
+
+        tab_index = self.window_handles.index(self.current_window_handle).__str__()
+        self.driverless.tab_user = "tab-" + tab_index
+        config = f"""
+                var connection = new connector("{self.driverless.socket.host}", {self.driverless.socket.port}, "{self.driverless.tab_user}")
+                connection.connect();
+                """
+        self.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument",
+                             {"source": "(function(){%s})()" % (utils_js + self.driverless.connection_js + config)})
+
+    def quit(self) -> None:
+        self.driverless.stop()
+        super().quit()
